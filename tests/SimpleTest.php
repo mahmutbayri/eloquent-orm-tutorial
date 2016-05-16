7258db90cc8611cb34f1c6928c18dc9f3acba4d0
Mahmut Bayri <mahmut.bayri@mynetgroup.com>
2016-05-16 08:32:32 +0300

    Added test files.

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class SimpleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_performs_a_simple_test()
+    {
+        $this->assertTrue(true);
+    }
+}
